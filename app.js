054eb4dc31ca4151bb330b1cedc33dec9316bf7b
UmarVD <muhammad.umar@venturedive.com>
2020-02-13 19:34:15 +0500

    Assignment-1: Update server for text file + logger

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+// const http = require("http");
 const http = require("http");
+const httpDispatcher = require("httpdispatcher");
+const { getFileName, readFile } = require("./utils/helpers");
 
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080);
+const dispatcher = new httpDispatcher();
+
+const logger = (request, response) => {
+  try {
+    console.log(request.url);
+    dispatcher.dispatch(request, response);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const server = http.createServer(logger);
+
+dispatcher.onGet("/", async function(req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  var fileName = await getFileName(".txt");
+  var text = readFile(`${fileName}`);
+  res.end(text);
+});
+
+server.listen(8000);
